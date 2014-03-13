707e1b965bda50d3044e2ff827e993d7349e2cf8
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-13 13:35:05 -0400

    js8

--- how much water your cup holds.js
+++ how much water your cup holds.js
@@ -9,7 +9,7 @@ const PI = 3.14159265;
 Height = prompt("Please enter the cups height"); 
 Height = parseInt(Height); // Convert to type 'int' 
 Radius = prompt("Please enter the raidus of the cup"); 
-Radius = parseInt(height); // Convert to type 'int' 
+Radius = parseInt(Height); // Convert to type 'int' 
 
 volume=(PI(Radius * Radius) * Height);
 
