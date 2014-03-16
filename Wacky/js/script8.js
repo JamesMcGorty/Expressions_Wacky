f18caf853c9f78bddac21320df9ebdbb33cb6e5d
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-16 15:59:56 -0400

    new project folder

---
+++ Wacky/js/script8.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+// James McGorty
+// Project: Expressions - Wacky
+
+// Volume of a cylinder which is PIr2H
+
+var wackyNames = ["Reid", "Guiddy", "Rod", "Stew", "Jennifer"];
+
+var enteredNum = prompt("Please number to get your name", 2);
+
+if (isNaN(enteredNum))
+	enteredNum = 0;
+else
+	enteredNum = parseInt(enteredNum);
+	
+var index = enteredNum % wackyNames.length;
+
+document.write("Your name is " + wackyNames[index]);
+
+console.log("The name is " + wackyNames[index]);
+
