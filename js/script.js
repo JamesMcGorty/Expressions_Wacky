705581f7c5cdf413647d973106f91e6274fab4cc
JamesMcGorty <jbmcgorty@gmail.com>
2014-03-16 17:38:51 -0400

    up to date

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 // JavaScript Document
+
 // James McGorty
+
 // Project: Expressions - Wacky
+
 
 // Volume of a cylinder which is PIr2H
 
+
+
 var wackyNames = ["Guiddy", "Reid", "Rod", "Stew", "Jennifer"];
+
+
 
 var enteredNum = prompt("Please number to get your name", 2);
 
+
+
 if (isNaN(enteredNum))
-	enteredNum = 0;
-else
-	enteredNum = parseInt(enteredNum);
 	
+	enteredNum = 0;
+
+else
+	
+	enteredNum = parseInt(enteredNum);
+
+
+
+
 var index = enteredNum % wackyNames.length;
 
+
+
 document.write("Your name is " + wackyNames[index]);
+
+
 
 console.log("The name is " + wackyNames[index]);
 
